fa471e2d17e49aa7bb12bfd2e1c94892051c6e21
Ricardo Miranda <rmiranda@datafact.com.ec>
2026-03-07 21:42:22 -0500

    feat: add database seeders for comprobantes and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-		$this->call(DocumentoSeeder::class);
+		//$this->call(DocumentoSeeder::class);
+                $this->call(ComprobanteSeeder::class);
+                $this->call(UserSeeder::class);
     }
 }
